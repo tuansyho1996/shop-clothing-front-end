28fa19dd2d044fcc4ddf0ec4684c06cbe0d2256c
tuansyho1996 <hosytuan101296@gmail.com>
2025-08-26 16:36:48 +0700

    auto sync

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,8 +1,6 @@
 import { getCategorySitemap } from "@/services/service.category"
 import { getProductSitemap } from "@/services/service.product"
 
-export const dynamic = 'force-dynamic' // hoặc bỏ hẳn
-
 export async function GET() {
     const domain = 'https://carnobon.com'
 
